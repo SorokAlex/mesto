23181bb31eb6cdf2c5a1d5892af86362c6ea6225
Alexander Sorokin <ASSorokin94fromWork@yandex.ru>
2023-03-17 09:43:09 +0300

    написать скрипт для попап

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+let userName = document.querySelector('.profile__user-name');
+let userJob = document.querySelector('.profile__user-job');
+let nameInput = document.querySelector('.popup__text_type_name');
+let jobInput = document.querySelector('.popup__text_type_job');
+const editButton = document.querySelector('.profile__edit-btn');
+const closeButton = document.querySelector('.popup__close-btn');
+const popUp = document.querySelector('.popup');
+
+userName.textContent = 'Жак-Ив Кусто';
+userJob.textContent = 'Исследователь океана';
+
+
+function popUpOpen() {
+    popUp.classList.add('popup_opened');
+    nameInput.value = userName.textContent;
+    jobInput.value = userJob.textContent;
+}
+function popUpClose() {
+  popUp.classList.remove('popup_opened');
+}
+
+function handleFormSubmit (evt) {
+  evt.preventDefault();
+  userName.textContent = nameInput.value;
+  userJob.textContent = jobInput.value;
+  popUp.classList.remove('popup_opened');
+}
+
+editButton.addEventListener('click', popUpOpen);
+closeButton.addEventListener('click', popUpClose);
+popUp.addEventListener('submit', handleFormSubmit);
