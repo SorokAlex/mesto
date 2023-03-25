5cb60ba3bbfde73afc2323ab06929d52f7aafbf6
Alexander Sorokin <ASSorokin94fromWork@yandex.ru>
2023-03-26 00:32:40 +0300

    Добавить функции открытия/закрытия попап, создания/удаления карточки, лайка/дизлайка

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,171 @@
+const initialCards = [
+  {
+    name: 'Карачаевск',
+    link: 'https://images.unsplash.com/photo-1588584922681-745a2223f72c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fCVEMCVCQSVEMCVCMCVEMSU4MCVEMCVCMCVEMSU4NyVEMCVCMCVEMCVCNSVEMCVCMiVEMCVCRXxlbnwwfHwwfHw%3D&w=1000&q=80',
+    alt: 'Храм на фоне горного хребта.'
+  },
+  {
+    name: 'Гора Эльбрус',
+    link: 'https://images.unsplash.com/photo-1669809503782-733587336cc0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80',
+    alt: 'Вид зелёное подножье и на гору Эльбрус.'
+  },
+  {
+    name: 'Домбай',
+    link: 'https://images.unsplash.com/photo-1640957551924-d14bbd5686bb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+    alt: 'Заснеженные склоны горы Домбай.'
+  },
+  {
+    name: 'Ставропольский край',
+    link: 'https://images.unsplash.com/photo-1620554918659-3f64bce0ac8e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+    alt: 'Монастырь бежевого цвете на фоне горы Бештау.'
+  },
+  {
+    name: 'Мыс Тобзина',
+    link: 'https://images.unsplash.com/photo-1629813366356-b0efcd428339?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80',
+    alt: 'Скала на берегу моря (мыс Тобзина).'
+  },
+  {
+    name: 'Владивосток',
+    link: 'https://images.unsplash.com/photo-1634887042266-3651a54b9a69?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+    alt: 'Храм на фоне моста.'
+  },
+];
+
+//Создаем перемменные для работы с попап "редактирования профиля"
+const popUpEditProfile = document.querySelector('.popup_type_edit-profile');
+const editButton = document.querySelector('.profile__edit-btn');
 let userName = document.querySelector('.profile__user-name');
 let userJob = document.querySelector('.profile__user-job');
 let nameInput = document.querySelector('.popup__text_type_name');
 let jobInput = document.querySelector('.popup__text_type_job');
-const editButton = document.querySelector('.profile__edit-btn');
-const closeButton = document.querySelector('.popup__close-btn');
-const popUp = document.querySelector('.popup');
+
+//Создаем пременные для работы с попап "новое место"
+const popUpAddPlaceCard = document.querySelector('.popup_type_add-place-card');
+const addButton = document.querySelector('.profile__add-btn');
+let titleInput = document.querySelector('.popup__text_type_title');
+let linkInput = document.querySelector('.popup__text_type_link');
 
 
-function popUpOpen() {
-    popUp.classList.add('popup_opened');
-    nameInput.value = userName.textContent;
-    jobInput.value = userJob.textContent;
-}
-function popUpClose() {
-  popUp.classList.remove('popup_opened');
-}
+//Создаем переменные для работы с попап "открытие картинки"
+const popUpFullSizePhoto = document.querySelector('.popup_type_full-size-photo');
+const popUpPhoto =  document.querySelector('.popup__figure-photo');
+const popUpPhotoCaption =  document.querySelector('.popup__figure-photo-caption');
 
-function handleFormSubmit (evt) {
+//Создаем переменные для шаблона и сетки мест
+const placesList = document.querySelector('.places-list');
+const placeTemplate = document.querySelector('#place-template').content;
+
+
+//Создаем переменную для работы со всеми попапами
+const popUps = document.querySelectorAll('.popup');
+
+//Создаем переменную для работы с кнопкой закрытия попавпов
+const closeButtons = document.querySelectorAll('.popup__close-btn');
+
+
+//Функция открытия попапа
+const openPopUp = (popUps) => {
+  popUps.classList.add("popup_opened");
+};
+
+//Функция закрытия попапа
+const closePopUp = (popUps) => {
+  popUps.classList.remove("popup_opened");
+};
+
+//Универсальное закрытие любого попапа
+closeButtons.forEach((button) => {
+  button.addEventListener('click', (evt) => {
+    const closestPopUp = evt.target.closest(".popup");
+    closePopUp(closestPopUp);
+  });
+});
+
+//Функция открытия попапа редактирования профиля
+editButton.addEventListener('click', () => {
+openPopUp(popUpEditProfile);
+nameInput.value = userName.textContent;
+jobInput.value = userJob.textContent;
+});
+
+//Функция сабмита для редактирования профиля
+const handleFormSubmitProfile = (evt) => {
   evt.preventDefault();
   userName.textContent = nameInput.value;
   userJob.textContent = jobInput.value;
-  popUp.classList.remove('popup_opened');
-}
+  closePopUp(popUpEditProfile);
+};
 
-editButton.addEventListener('click', popUpOpen);
-closeButton.addEventListener('click', popUpClose);
-popUp.addEventListener('submit', handleFormSubmit);
+//Функция открытия попапа добавления нового места
+addButton.addEventListener('click', () => {
+  openPopUp(popUpAddPlaceCard);
+  titleInput.value = '';
+  linkInput.value = '';
+});
+
+//Функция сабмита для создания нового места
+const handleFormSubmitPlace = (evt) => {
+  evt.preventDefault();
+  renderPlace({
+    name: titleInput.value,
+    link: linkInput.value,
+    alt: `${titleInput.value}`,
+  });
+  closePopUp(popUpAddPlaceCard);
+};
+
+//Открытие полноразмерного изображения
+const handleFullSizePhoto = (photo) => {
+  photo.addEventListener('click', (evt) => {
+    openPopUp(popUpFullSizePhoto);
+    popUpPhoto.src = photo.src;
+    popUpPhoto.alt = photo.alt;
+    popUpPhotoCaption.textContent = evt.target.closest('.place-card').textContent;
+  });
+};
+
+//Функция добавления нового места в начало
+let renderPlace = (placeCard) => {
+  placesList.prepend(createPlace(placeCard));
+};
+
+//Управление удалением карточки
+const handleDeleteButtonPlace = (deleteButtonPlace) => {
+  deleteButtonPlace.addEventListener('click', (evt) => {
+    evt.target.closest('.place-card').remove();
+  });
+};
+
+//Управление лайком карточки
+const handleLikeButtonPlace = (likeButtonPlace) => {
+  likeButtonPlace.addEventListener('click', (evt) => {
+    evt.target.classList.toggle('place-card__like-btn_type_active');
+  });
+};
+
+//Функция создания карточки
+const createPlace = (place) => {
+  const newPlace = placeTemplate.cloneNode(true);
+  const deleteButtonPlace = newPlace.querySelector('.place-card__delete-btn');
+  const likeButtonPlace =  newPlace.querySelector('.place-card__like-btn');
+  let placeTitle = newPlace.querySelector('.place-card__title');
+  let placePhoto = newPlace.querySelector('.place-card__photo');
+  placeTitle.textContent = place.name;
+  placePhoto.setAttribute('src', place.link);
+  placePhoto.setAttribute('alt', place.alt);
+
+  handleDeleteButtonPlace(deleteButtonPlace);
+  handleLikeButtonPlace(likeButtonPlace);
+  handleFullSizePhoto(placePhoto);
+
+  return newPlace;
+};
+
+//Карточки в порядке массива
+initialCards.forEach((placeCard) => {
+  placesList.append(createPlace(placeCard));
+});
+
+//Слушатели событий сохранения/отправки заполненной формы
+popUpEditProfile.addEventListener('submit', handleFormSubmitProfile);
+popUpAddPlaceCard.addEventListener('submit', handleFormSubmitPlace);
